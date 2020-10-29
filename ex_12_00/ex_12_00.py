d20a7461433351afd00afc30fe4478c5a4899587
bswole <smk1986@yahoo.com>
2020-10-29 10:23:54 -0600

    create and complete ex 12.0

---
+++ ex_12_00/ex_12_00.py
@@ -0,0 +1,16 @@
+import socket
+
+# https://www.youtube.com/watch?v=dWLdI143W-g&ab_channel=freeCodeCampConcepts
+print('Exercise 12.0')
+
+mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+mysock.connect(('data.pr4e.org', 80))
+cmd = 'GET http://data.pr4e.org/romeo.txt HTTP/1.0\r\nHost: data.pr4e.org\r\n\r\n'.encode()
+mysock.send(cmd)
+
+while True:
+    data = mysock.recv(512)
+    if len(data) < 1:
+        break
+    print(data.decode())
+mysock.close()
